fadf94752fdce5ab81320b28888ec4418fda8f5c
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-06-06 23:06:34 +0100

    Initial Commit

---
+++ XCode9 testTests/XCode9_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  XCode9_testTests.swift
+//  XCode9 testTests
+//
+//  Created by Trevor Doodes on 06/06/2017.
+//  Copyright © 2017 Ironworks Media Limited. All rights reserved.
+//
+
+import XCTest
+@testable import XCode9_test
+
+class XCode9_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
